cf933af4dc4ef7f13949edb5d8f0b59f445d2cb2
root <okekefeargod@gmail.com>
2023-12-23 12:57:38 -0800

    6. DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,18 @@
 import uuid
 from datetime import datetime
 import models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+
+
+Base = declarative_base()
 
 
 class BaseModel():
     ''' Defines all common attributes/methods for other classes '''
+    id = Column(String(60), nullable=False, primary_key=True, unique=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
         ''' Instantiate an object with id, created_at and updated_at
@@ -35,7 +43,6 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-        models.storage.new(self)
 
     def __str__(self):
         '''return string representation'''
@@ -46,6 +53,7 @@ class BaseModel():
         '''updates the public instance attribute updated_at
         with the current datetime'''
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -54,4 +62,13 @@ class BaseModel():
         mydict["__class__"] = self.__class__.__name__
         mydict["created_at"] = self.created_at.isoformat()
         mydict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in mydict.keys():
+            del mydict['_sa_instance_state']
         return mydict
+
+    def delete(self):
+        """
+        To  delete the current instance from the storage
+        (models.storage) by calling the method delete
+        """
+        models.storage.delete(self)
